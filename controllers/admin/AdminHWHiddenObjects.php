2e9454b23c673db4a2474098a92b7e628ea209e5
sas-adilis <contact@adilis.fr>
2023-05-26 10:06:00 +0200

    Attempted to load class "Db" from namespace

---
+++ controllers/admin/AdminHWHiddenObjects.php
@@ -0,0 +1,18 @@
+<?php
+
+use Adilis\HiddenObjects\Controllers\Admin\AdminHiddenObjectsController;
+
+/**
+ * 2016 Adilis
+ *
+ * Make your shop interactive for Easter: hide objects and ask your customers to find them in order to win a
+ * discount coupon. Make your brand stand out by offering an original game: a treasure hunt throughout your products.
+ *
+ *  @author    Adilis <support@adilis.fr>
+ *  @copyright 2016 SAS Adilis
+ *  @license   http://www.adilis.fr
+ */
+
+class AdminHWHiddenObjectsController extends AdminHiddenObjectsController
+{
+}
