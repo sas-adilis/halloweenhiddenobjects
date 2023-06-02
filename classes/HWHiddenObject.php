6f2ab3947c39fcf41af0b1462d4ea715b0347b01
sas-adilis <contact@adilis.fr>
2023-06-02 17:00:54 +0200

    validator issues

--- classes/HWHiddenObject.php
+++ classes/HWHiddenObject.php
@@ -1,18 +1,15 @@
 <?php
-
-use Adilis\HiddenObjects\Classes\HiddenObject;
-use Adilis\HiddenObjects\Classes\HiddenObjectInterface;
-
 /**
  * 2016 Adilis
- *
- * Make your shop interactive for Easter: hide objects and ask your customers to find them in order to win a
- * discount coupon. Make your brand stand out by offering an original game: a treasure hunt throughout your products.
  *
  *  @author    Adilis <support@adilis.fr>
  *  @copyright 2016 SAS Adilis
  *  @license   http://www.adilis.fr
  */
+
+use Adilis\HiddenObjects\Classes\HiddenObject;
+use Adilis\HiddenObjects\Classes\HiddenObjectInterface;
+
 class HWHiddenObject extends HiddenObject implements HiddenObjectInterface
 {
     public function getPrefix(): string
--- controllers/front/found.php
+++ controllers/front/found.php
@@ -1,17 +1,14 @@
 <?php
-
-use Adilis\HiddenObjects\Controllers\Front\HiddenObjectsFoundModuleFrontController;
-
 /**
  * 2016 Adilis
- *
- * Make your shop interactive for Easter: hide objects and ask your customers to find them in order to win a
- * discount coupon. Make your brand stand out by offering an original game: a treasure hunt throughout your products.
  *
  *  @author    Adilis <support@adilis.fr>
  *  @copyright 2016 SAS Adilis
  *  @license   http://www.adilis.fr
  */
+
+use Adilis\HiddenObjects\Controllers\Front\HiddenObjectsFoundModuleFrontController;
+
 class HalloweenHiddenObjectsFoundModuleFrontController extends HiddenObjectsFoundModuleFrontController
 {
 }
